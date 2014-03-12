3d0159e5c33a96c77db5ab61691850cea088854e
Andrew Metcalf <andrew@stripe.com>
2014-03-12 11:01:24 -0700

    rubocop configure

--- configure.rb
+++ configure.rb
@@ -4,19 +4,20 @@ require 'shellwords'
 require 'FileUtils'
 
 LINK_MAP = {
-    "bashrc" => ".bashrc",
-    "coffeelint-config.json" => ".coffeelint.json",
-    "emacs" => ".emacs",
-    "git-completion.sh" => ".git-completion.sh",
-    "emacs.d" => ".emacs.d",
-    "inputrc" => ".inputrc",
-    "ssh-config" => ".ssh-config"
+    'bashrc' => '.bashrc',
+    'coffeelint-config.json' => '.coffeelint.json',
+    'emacs' => '.emacs',
+    'git-completion.sh' => '.git-completion.sh',
+    'emacs.d' => '.emacs.d',
+    'inputrc' => '.inputrc',
+#    'ssh-config' => '.ssh/config',
+    'rubocop.yml' => '.rubocop.yml',
 }
 
 DIRECTORIES = [
-  ".emacs_autosaves",
-  ".emacs_backups",
-  ".ssh/sockets"
+  '.emacs_autosaves',
+  '.emacs_backups',
+  '.ssh/sockets'
 ]
 
 BREW_PACKAGES = [
@@ -24,6 +25,7 @@ BREW_PACKAGES = [
   'wget',
   'mobile-shell',
   'go',
+  'grep --default-names',
 ]
 
 GO_DEPS = [
@@ -32,8 +34,8 @@ GO_DEPS = [
   'code.google.com/p/rog-go/exp/cmd/godef',
 ]
 
-def yes_or_no_prompt(prompt, opts={})
-  yn = opts[:default_y] ? "Y/n" : "y/N"
+def yes_or_no_prompt(prompt, opts = {})
+  yn = opts[:default_y] ? 'Y/n' : 'y/N'
   print "#{prompt} (#{yn}) "
   reply = STDIN.gets.chomp
   if opts[:default_y]
@@ -51,12 +53,18 @@ end
 def check_dependencies
   `which brew`
   unless $?.success?
-    puts "Please install homebrew"
+    puts 'Please install homebrew'
+    return false
+  end
+  unless `brew update`.match(/Updated Homebrew|Already up-to-date/)
+    puts 'Could not update Homebrew'
     return false
   end
   unless `brew doctor`.match(/ready to brew/)
-    puts "Homebrew is unhappy.  Please run 'brew doctor'"
-    return false
+    unless yes_or_no_prompt('Homebrew looks unhappy (see brew doctor output above).  ' +
+        'Should I continue?')
+      return false
+    end
   end
 
   true
@@ -70,8 +78,8 @@ def install!
   install_core
   configure_defaults
 
-  puts "Note: You need to install fonts manually now."
-  puts "Configure complete!"
+  puts 'Note: You need to install fonts manually now.'
+  puts 'Configure complete!'
 end
 
 def mk_directories
@@ -83,22 +91,22 @@ def mk_directories
 end
 
 def link_files
-    LINK_MAP.each do |source, target|
-        source = File.expand_path("./#{source}")
-	target = File.expand_path("~/#{target}")
-        if File.symlink?(target) && File.readlink(target) == source
-          next
-        elsif File.exists?(target)
-          if yes_or_no_prompt("#{target} already exists. Back it up and replace it?")
-            backup_file(target)
-          else
-            puts "Skipping #{source}"
-            next
-          end
-        end
-        puts "Linking #{target} to #{source}"
-        File.symlink(source, target)
+  LINK_MAP.each do |source, target|
+    source = File.expand_path("./#{source}")
+    target = File.expand_path("~/#{target}")
+    if File.symlink?(target) && File.readlink(target) == source
+      next
+    elsif File.exists?(target)
+      if yes_or_no_prompt("#{target} already exists. Back it up and replace it?")
+        backup_file(target)
+      else
+        puts "Skipping #{source}"
+        next
+      end
     end
+    puts "Linking #{target} to #{source}"
+    File.symlink(source, target)
+  end
 end
 
 def install_core
@@ -106,6 +114,7 @@ def install_core
   `sudo gem update --system`
 
   puts 'Installing homebrew packages'
+  `brew tap homebrew/dupes`
   BREW_PACKAGES.each do |pkg|
     `brew install #{pkg}`
   end
